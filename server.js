51bda2223a36a7c1a7aa8de74aeff2056cdf9576
Ashar <74727596+Asharshahid@users.noreply.github.com>
2024-02-28 19:28:36 +0500

    cors added

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ import './database/db.js'
 import express from "express";
 import router from "./router/route.js"
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 app.use(router)
 
 
