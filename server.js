0193d7a0ba67708736c2824488b96d40223b59dc
Ashar <74727596+Asharshahid@users.noreply.github.com>
2024-03-04 15:25:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ import router from "./router/route.js"
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-app.use(cors());
+
 const app = express()
 app.use(express.json())
+app.use(cors());
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}));
 app.use(router)
