3362b4d66e7577f604069e8bf528e7bc26f5319d
Ashar <74727596+Asharshahid@users.noreply.github.com>
2024-03-04 22:06:06 +0500

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -20,7 +20,7 @@ export async function check(req, res){
         const token = await jwt.sign({_id:"65dc5e5ff020baebdaad8515"}, 'ubit123456789');
                 res.cookie("jwt",token,{
                     // expires:new Date(Date.now()+5000),
-                    httpOnly:true,
+                    //httpOnly:true,
                     //secure: true,
                     //sameSite: 'None'
                 })
